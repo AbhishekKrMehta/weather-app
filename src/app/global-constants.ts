944aae1a27f4211fb7ff47ffb831e9cfddb0378b
Abhishek <abhishek.kr.mehta1@gmail.com>
2021-03-18 22:19:17 +0100

    fix alignment of columns

--- src/app/global-constants.ts
+++ src/app/global-constants.ts
@@ -1,6 +1,5 @@
 export class GlobalConstants {
-  public static readonly apiKey: string = 'a12ba9e6391352df026c56f367f299a9'; // TODO: use this
-  // public static readonly apiKey: string = 'eb35b0d62d965ee519f675c144eaa1e9';
-  public static readonly numberOfHoursInForcast: number = 10;
+  // public static readonly apiKey: string = 'a12ba9e6391352df026c56f367f299a9'; // TODO: use this
+  public static readonly apiKey: string = 'eb35b0d62d965ee519f675c144eaa1e9';
   public static readonly forecastDisplayedColumns: Array<string> = ['time', 'averageTemperature', 'windSpeed'];
 }
